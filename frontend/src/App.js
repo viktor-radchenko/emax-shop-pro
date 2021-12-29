8296ab67a7f7ca2af8ccf43baff3f5d1e0e794d5
viktor <radchenkovictor@yahoo.com>
2021-12-29 02:12:32 +0200

    added initial react app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{process.env.REACT_APP_APP_NAME}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
